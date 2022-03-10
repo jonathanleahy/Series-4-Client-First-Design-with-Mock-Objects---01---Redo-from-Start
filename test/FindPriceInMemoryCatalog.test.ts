a2cd99e7ddbc9ccca9cbd7584dd209365b8402a4
Jon Leahy <jonathanl@newtimesystems.com>
2022-03-10 17:04:29 +0000

    We can find a price if the price is there to be found

---
+++ test/FindPriceInMemoryCatalog.test.ts
@@ -0,0 +1,24 @@
+import {Price} from "./Price";
+
+describe("FindPriceInMemoryCatalog", () => {
+
+    test('Product Found', () => {
+            const foundPrice: Price = Price.cents(795)
+            const catalog: InMemoryCatalog = new InMemoryCatalog(new Map<string, Price>([["12345", foundPrice]]))
+            expect(foundPrice).toEqual(catalog.findPrice("12345"))
+        }
+    )
+
+})
+
+export class InMemoryCatalog {
+    private pricesByBarCode: Map<string, Price>
+
+    public constructor(pricesByBarCode: Map<string, Price>) {
+        this.pricesByBarCode = pricesByBarCode;
+    }
+
+    public findPrice(barcode: string): Price {
+        return this.pricesByBarCode.get(barcode)
+    }
+}
